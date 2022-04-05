372c2bfcdc048aabe8edafdab681f068cb2ef5ed
RiskaDifani <idriska48@gmail.com>
2022-04-05 10:27:44 +0700

    wallpaper

---
+++ lib/views/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_wallpaper/widgets/widget.dart';
+class Home extends StatefulWidget {
+
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: brandName(),
+        elevation: 0.0,
+      ),
+    );
+  }
+}
